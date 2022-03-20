37d713f8f493db1f9d60fa065e7c8314f263cd82
Mahmud Hasan <mhr.swe.522@gmail.com>
2022-03-20 09:33:42 +0600

    Create API

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,21 +1,39 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
 
 
-# class Carnival(http.Controller):
-#     @http.route('/carnival/carnival', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+from odoo import http
+from odoo.http import request, Response
+from json import dumps
+from odoo.tools import date_utils
 
-#     @http.route('/carnival/carnival/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('carnival.listing', {
-#             'root': '/carnival/carnival',
-#             'objects': http.request.env['carnival.carnival'].search([]),
-#         })
 
-#     @http.route('/carnival/carnival/objects/<model("carnival.carnival"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('carnival.object', {
-#             'object': obj
-#         })
+class Carnival(http.Controller):
+
+    @http.route('/carnival/get/event_list', auth='public', type='http', csrf=False, methods=['GET'], cors='*')
+    def event_list(self, **kwarg):
+        result=[]
+        events = request.env['event'].sudo().search([])
+        for line_event in events:
+            data = {
+                "name":line_event.name,
+                "organizers":line_event.organizers,
+            }
+            result.append(data)
+        json_event_data = dumps(result, default=date_utils.json_default)
+        return Response(json_event_data, headers={'content_type': 'application/json'}, status=200)
+
+    @http.route('/carnival/get/stall_list', auth='public', type='http', csrf=False, methods=['GET'], cors='*')
+    def stall_list(self, **kwarg):
+        result = []
+        stalls = request.env['event'].sudo().search([])
+        for line_event_stalls_information in stalls:
+            data = {
+                "name": line_event_stalls_information.name,
+                "organizers": line_event_stalls_information.position,
+            }
+            result.append(data)
+        json_stall_data = dumps(result, default=date_utils.json_default)
+        return Response(json_stall_data, headers={'content_type': 'application/json'}, status=200)
+
+
+
