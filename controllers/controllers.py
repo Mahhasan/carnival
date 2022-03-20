b8020bf026272b0b9819fbf6f2db76bfbca2f4cf
Mahmud Hasan <mhr.swe.522@gmail.com>
2022-03-20 17:13:35 +0600

    Another API Created

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -22,18 +22,45 @@ class Carnival(http.Controller):
         json_event_data = dumps(result, default=date_utils.json_default)
         return Response(json_event_data, headers={'content_type': 'application/json'}, status=200)
 
-    @http.route('/carnival/get/stall_list', auth='public', type='http', csrf=False, methods=['GET'], cors='*')
-    def stall_list(self, **kwarg):
+    @http.route('/carnival/get/stalls_list', auth='public', type='http', csrf=False, methods=['GET'], cors='*')
+    def stalls_list(self, **kwarg):
         result = []
-        stalls = request.env['event'].sudo().search([])
+        stalls = request.env['event.stalls.information'].sudo().search([])
         for line_event_stalls_information in stalls:
             data = {
                 "name": line_event_stalls_information.name,
-                "organizers": line_event_stalls_information.position,
+                "exhibitor": line_event_stalls_information.exhibitor,
             }
             result.append(data)
-        json_stall_data = dumps(result, default=date_utils.json_default)
-        return Response(json_stall_data, headers={'content_type': 'application/json'}, status=200)
+        json_event_stalls_information_data = dumps(result, default=date_utils.json_default)
+        return Response(json_event_stalls_information_data, headers={'content_type': 'application/json'}, status=200)
+
+    @http.route('/carnival/get/halls_list', auth='public', type='http', csrf=False, methods=['GET'], cors='*')
+    def halls_list(self, **kwarg):
+        result = []
+        halls = request.env['event.halls.information'].sudo().search([])
+        for line_event_halls_information in halls:
+            data = {
+                "name": line_event_halls_information.name,
+                "code": line_event_halls_information.code,
+            }
+            result.append(data)
+        json_event_halls_information_data = dumps(result, default=date_utils.json_default)
+        return Response(json_event_halls_information_data, headers={'content_type': 'application/json'}, status=200)
+
+    @http.route('/carnival/get/halls_list', auth='public', type='http', csrf=False, methods=['GET'], cors='*')
+    def halls_list(self, **kwarg):
+        result = []
+        halls  = request.env['event.halls.information'].sudo().search([])
+        for line_event_halls_information in halls:
+            data = {
+                "name": line_event_halls_information.name,
+                "code": line_event_halls_information.code,
+            }
+            result.append(data)
+        json_event_halls_information_data = dumps(result, default=date_utils.json_default)
+        return Response(json_event_halls_information_data, headers={'content_type': 'application/json'}, status=200)
+
 
 
 
